cfb2a3e55c08ef2e4b85184056f632f8363768a6
Jim Garlick <garlick.jim@gmail.com>
2017-11-17 10:56:51 -0800

    lib/sigcert: add signing certificate class
    RFC 15 calls for users to sign J so that other users,
    including the instance owner, cannot forge run requests
    to the IMP.
    
    One mechanism that satisfies this requirement is
    libsodium's public key signatures based on ed25519.
    This is a good starting point since we are already using
    an instance owner's CURVE keys in Flux to protect
    overlay network peer connections, and thus are already
    dependent on libsodium.
    
    0MQ introduced a certificate format for its socket
    security called zcert (czmq.zeromq.org/manual:zcert).
    These certificates cannot be used for public key
    signatures because libsodium uses a different key format
    for signing keys.  Thus, we introduce a new class
    called flux_sigcert that is similar to zcert, but not
    dependent on czmq.  The on-disk certificate format
    uses TOML instead of ZPL format, and base64 encoding
    instead of Z85 for ASCII armor.
    
    This class is intended to be expanded to support JSON
    signing, certificate signing, and certificate serialization
    as JSON, however in this iteration only detached blob signing
    is supported.

---
+++ src/lib/sigcert.h
@@ -0,0 +1,66 @@
+#ifndef _FLUX_SIGCERT_H
+#define _FLUX_SIGCERT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Certificate class for signing/verification.
+ *
+ * Keys can be loaded/stored from TOML based certificate files.
+ * The "secret" version of the certificate contains everything.
+ * The "public" version of the certificate contains all but private keys.
+ * The public version is distinguished by a .pub extension (like ssh keys).
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct flux_sigcert;
+
+/* Destroy cert.
+ */
+void flux_sigcert_destroy (struct flux_sigcert *cert);
+
+/* Create cert with new keys
+ */
+struct flux_sigcert *flux_sigcert_create (void);
+
+/* Load cert from file 'name', falling back to 'name.pub'.
+ */
+struct flux_sigcert *flux_sigcert_load (const char *name);
+
+/* Store cert to 'name' and 'name.pub'.
+ */
+int flux_sigcert_store (struct flux_sigcert *cert, const char *name);
+
+/* Return true if two certificates have the same keys.
+ */
+bool flux_sigcert_equal (struct flux_sigcert *cert1,
+                         struct flux_sigcert *cert2);
+
+/* Return a detached signature (base64 string) over buf, len.
+ * Caller must free.
+ */
+char *flux_sigcert_sign (struct flux_sigcert *cert,
+                         uint8_t *buf, int len);
+
+/* Verify a detached signature (base64 string) over buf, len.
+ * Returns 0 on success, -1 on failure.
+ */
+int flux_sigcert_verify (struct flux_sigcert *cert,
+                         const char *signature, uint8_t *buf, int len);
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_FLUX_SIGCERT_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
