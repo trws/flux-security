60e050db353b51b358abd6d8032a88eab7172fa4
Jim Garlick <garlick.jim@gmail.com>
2018-02-20 11:48:01 -0800

    libtomlc99/test: cover utf8 conversion functions
    Tests covering toml_utf8_to_ucs() multibyte input
    are set to XFAIL pending a bug fix.

--- src/libtomlc99/toml_tap.c
+++ src/libtomlc99/toml_tap.c
@@ -9,6 +9,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <string.h>
+#include <locale.h>
 
 #include "src/libtap/tap.h"
 #include "toml.h"
@@ -316,6 +317,112 @@ void parse_extra (void)
 
 }
 
+void check_ucs_to_utf8 (void)
+{
+    char buf[6];
+    int64_t code;
+    int errors;
+
+    errors = 0;
+    for (code = 0xd800; code <= 0xdfff; code++)
+        if (toml_ucs_to_utf8 (code, buf) != -1)
+            errors++;
+    ok (errors == 0,
+        "ucs_to_utf8: UTF-16 surrogates are rejected");
+
+    errors = 0;
+    for (code = 0xfffe; code <= 0xffff; code++)
+        if (toml_ucs_to_utf8 (code, buf) != -1)
+            errors++;
+    ok (errors == 0,
+        "ucs_to_utf8: UCS non-characters are rejected");
+
+    ok (toml_ucs_to_utf8 (-42, buf) < 0,
+        "ucs_to_utf8: UCS negative code is rejected");
+
+    errors = 0;
+    for (code = 0; code <= 0x7f; code++)
+        if (toml_ucs_to_utf8 (code, buf) != 1 || buf[0] != code)
+            errors++;
+    ok (errors == 0,
+        "ucs_to_utf8: 1 byte codes convert directly to UTF8");
+
+    /* Check boundary values against results from:
+     *   http://www.ltg.ed.ac.uk/~richard/utf-8.cgi
+     */
+
+    ok (toml_ucs_to_utf8 (0x80, buf) == 2 && !memcmp (buf, "\xc2\x80", 2),
+        "ucs_to_utf8: 0x80 converted to 2-char UTF8");
+    ok (toml_ucs_to_utf8 (0x7ff, buf) == 2 && !memcmp (buf, "\xdf\xbf", 2),
+        "ucs_to_utf8: 0x7ff converted to 2-char UTF8");
+
+    ok (toml_ucs_to_utf8 (0x800, buf) == 3 && !memcmp (buf, "\xe0\xa0\x80", 3),
+        "ucs_to_utf8: 0x800 converted to 3-char UTF8");
+    ok (toml_ucs_to_utf8 (0xfffd, buf) == 3 && !memcmp (buf, "\xef\xbf\xbd", 3),
+        "ucs_to_utf8: 0xfffd converted to 3-char UTF8");
+
+    ok (toml_ucs_to_utf8 (0x10000, buf) == 4
+        && !memcmp (buf, "\xf0\x90\x80\x80", 4),
+        "ucs_to_utf8: 0x10000 converted to 4-char UTF8");
+    ok (toml_ucs_to_utf8 (0x1fffff, buf) == 4
+        && !memcmp (buf, "\xf7\xbf\xbf\xbf", 4),
+        "ucs_to_utf8: 0x1ffff converted to 4-char UTF8");
+
+    ok (toml_ucs_to_utf8 (0x200000, buf) == 5
+        && !memcmp (buf, "\xf8\x88\x80\x80\x80", 5),
+        "ucs_to_utf8: 0x200000 converted to 5-char UTF8");
+    ok (toml_ucs_to_utf8 (0x3ffffff, buf) == 5
+        && !memcmp (buf, "\xfb\xbf\xbf\xbf\xbf", 5),
+        "ucs_to_utf8: 0x3ffffff converted to 5-char UTF8");
+
+    ok (toml_ucs_to_utf8 (0x4000000, buf) == 6
+        && !memcmp (buf, "\xfc\x84\x80\x80\x80\x80", 6),
+        "ucs_to_utf8: 0x4000000 converted to 6-char UTF8");
+    ok (toml_ucs_to_utf8 (0x7fffffff, buf) == 6
+        && !memcmp (buf, "\xfd\xbf\xbf\xbf\xbf\xbf", 6),
+        "ucs_to_utf8: 0x7fffffff converted to 6-char UTF8");
+}
+
+void check_utf8_to_ucs (void)
+{
+    int64_t code;
+
+    /* Reverse above values
+     */
+
+    ok (toml_utf8_to_ucs ("\x0", 1, &code) == 1 && code == 0,
+        "utf8_to_ucs: 0 converted from 1-char UTF8");
+    ok (toml_utf8_to_ucs ("\x7f", 1, &code) == 1 && code == 0x7f,
+        "utf8_to_ucs: 0x7f converted from 1-char UTF8");
+
+    todo ("https://github.com/cktan/tomlc99/issues/7");
+    ok (toml_utf8_to_ucs ("\xc2\x80", 2, &code) == 2 && code == 0x80,
+        "utf8_to_ucs: 0x80 converted from 2-char UTF8");
+    ok (toml_utf8_to_ucs ("\xdf\xbf", 2, &code) == 2 && code == 0x7ff,
+        "utf8_to_ucs: 0x7ff converted from 2-char UTF8");
+
+    ok (toml_utf8_to_ucs ("\xe0\xa0\x80", 3, &code) == 3 && code == 0x800,
+        "utf8_to_ucs: 0x800 converted from 3-char UTF8");
+    ok (toml_utf8_to_ucs ("\xef\xbf\xbd", 3, &code) == 3 && code == 0xfffd,
+        "utf8_to_ucs: 0xfffd converted from 3-char UTF8");
+
+    ok (toml_utf8_to_ucs ("\xf0\x90\x80\x80", 4, &code) == 4 && code == 0x10000,
+        "utf8_to_ucs: 0x10000 converted from 4-char UTF8");
+    ok (toml_utf8_to_ucs ("\xf0\x90\x80\x80", 4, &code) == 4 && code == 0x10000,
+        "utf8_to_ucs: 0x10000 converted from 4-char UTF8");
+
+    ok (toml_utf8_to_ucs ("\xf8\x88\x80\x80\x80", 5, &code) == 5 && code == 0x200000,
+        "utf8_to_ucs: 0x200000 converted from 5-char UTF8");
+    ok (toml_utf8_to_ucs ("\xfb\xbf\xbf\xbf\xbf", 5, &code) == 5 && code == 0x3ffffff,
+        "utf8_to_ucs: 0x3ffffff converted from 5-char UTF8");
+
+    ok (toml_utf8_to_ucs ("\xfc\x84\x80\x80\x80\x80", 6, &code) == 6 && code == 0x4000000,
+        "utf8_to_ucs: 0x4000000 converted from 6-char UTF8");
+    ok (toml_utf8_to_ucs ("\xfd\xbf\xbf\xbf\xbf\xbf", 6, &code) == 6 && code == 0x7fffffff,
+        "utf8_to_ucs: 0x7fffffff converted from 6-char UTF8");
+    end_todo;
+}
+
 int main (int argc, char *argv[])
 {
     plan (NO_PLAN);
@@ -326,6 +433,9 @@ int main (int argc, char *argv[])
     parse_bad_input ();
 
     parse_extra ();
+
+    check_ucs_to_utf8 ();
+    check_utf8_to_ucs ();
 
     done_testing ();
 }
