b6e70b29e3991daeef27064855707ec07fff6523
Jim Garlick <garlick.jim@gmail.com>
2018-11-01 06:14:00 -0700

    libutil: define BASE64_DECODE_SIZE in macros.h
    It is convenient to know the maximum buffer size
    to decode a base64 string of some size.  Copy the
    BASE64_DECODE_SIZE macro from flux-core.

---
+++ src/libutil/macros.h
@@ -0,0 +1,13 @@
+#ifndef LIBUTIL_MACROS_H
+#define LIBUTIL_MACROS_H 1
+
+/* Maximum size of buffer needed to decode a base64 string of length 'x',
+ * where 4 characters are decoded into 3 bytes.  Add 3 bytes to ensure a
+ * partial 4-byte chunk will be accounted for during integer division.
+ * This size is safe for use with all (4) libsodium base64 variants.
+ * N.B. unlike @dun's base64 implementation from the munge project,
+ * this size does not include room for a \0 string terminator.
+ */
+#define BASE64_DECODE_SIZE(x) ((((x) + 3) / 4) * 3)
+
+#endif
