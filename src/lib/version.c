5ab815944cc6bc36921e2ec114db9cfee39329d8
Jim Garlick <garlick.jim@gmail.com>
2018-11-08 19:05:01 -0800

    libflux/version: add version accessors
    Add <flux/security/version.h> which defines the following
    macros for accessing the flux-security source version
    
    FLUX_SECURITY_VERSION_STRING // string, e.g. "0.10.0"
    
    FLUX_SECURITY_VERSION_MAJOR  // numeric e.g. 0
    FLUX_SECURITY_VERSION_MINOR  // numeric e.g. 10
    FLUX_SECURITY_VERSION_PATCH  // numeric e.g. 0
    FLUX_SECURITY_VERSION_HEX    // 3 byte numeric, e.g. 160 (0x0A0)
    
    and the following functions which return:
    
    flux_security_version_string () // string value above
    flux_security_version ()        // all four numeric values above
    
    The version.h header is generated from version.h.in
    at configure time.  Also add version.h to .gitignore.

---
+++ src/lib/version.c
@@ -0,0 +1,50 @@
+/*****************************************************************************\
+ *  Copyright (c) 2018 Lawrence Livermore National Security, LLC.  Produced at
+ *  the Lawrence Livermore National Laboratory (cf, AUTHORS, DISCLAIMER.LLNS).
+ *  LLNL-CODE-658032 All rights reserved.
+ *
+ *  This file is part of the Flux resource manager framework.
+ *  For details, see https://github.com/flux-framework.
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2.1 of the license,
+ *  or (at your option) any later version.
+ *
+ *  Flux is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *  See also:  http://www.gnu.org/licenses/
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "version.h"
+
+const char *flux_security_version_string (void)
+{
+    return FLUX_SECURITY_VERSION_STRING;
+}
+
+int flux_security_version (int *major, int *minor, int *patch)
+{
+    if (major)
+        *major = FLUX_SECURITY_VERSION_MAJOR;
+    if (minor)
+        *minor = FLUX_SECURITY_VERSION_MINOR;
+    if (patch)
+        *patch = FLUX_SECURITY_VERSION_PATCH;
+    return FLUX_SECURITY_VERSION_HEX;
+}
+
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
