688b6456d40d70e4d53afcab094e4f1de116a9f4
Jim Garlick <garlick.jim@gmail.com>
2019-01-03 13:38:32 -0800

    convert copyright notice to new SPDX LGPLv3

--- t/src/certutil.c
+++ t/src/certutil.c
@@ -1,3 +1,13 @@
+/************************************************************\
+ * Copyright 2017 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
 /* certutil.c - get/put cert metadata
  *
  * Usage: certutil certname get key
